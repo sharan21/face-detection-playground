d529c97c7324c7fc4c1fe1ed7a7c6c7836120f41
sharan21 <sharan.n21@gmail.com>
2019-02-06 19:30:23 +0530

    modified

--- detectFace.py
+++ detectFace.py
@@ -1,41 +1,80 @@
 import numpy as np
 import cv2 as cv
-import tqdm as tq
 
 
-cascade_path = './haarcascade_frontalface_default.xml'
-path = 'sample_pictures/group1.jpg'
+class faceDetect:
 
-def detectFace(cascade_path, path):
+    cascade_path = 'haarcascade_frontalface_default.xml'
+    path = 'sample_pictures/group1.jpg'
+    inputwidth = 25;
+    inputheight = 25;
 
-    img = cv.imread(path)
-    face_cascade = cv.CascadeClassifier(cascade_path)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    crop_img = []
-    gray_img = []
 
-    faces = face_cascade.detectMultiScale(gray, 1.5, 1)
-    for (x,y,w,h) in faces:
-        #  cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        crop_img.append(img[y:y+h, x:x+w])
-        gray_img.append(img[y:y + h, x:x + w])
+    def __init__(self, instance):
+        self.instance = instance
+        print("object initialised")
 
-    print("writing cropped images into directories")
+    def detectFaceOfImage(self,cascade_path, path):
 
-    for i in range(0, len(crop_img), 1):
-        filename = "cropped_images/img{}.jpg".format(i)
-        filenamegray = "grayimg{}.jpg".format(i)
-        cv.imwrite(filename, crop_img[i])
+        img = cv.imread(path)
+        face_cascade = cv.CascadeClassifier(cascade_path)
+        crop_img = []
+        gray_img = []
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    print("done saving images")
+        faces = face_cascade.detectMultiScale(gray, 1.5, 1)
+        for (x, y, w, h) in faces:
+            # cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            crop_img.append(img[y:y + h, x:x + w])
+            gray_img.append(img[y:y + h, x:x + w])
 
-# MAIN
 
-detectFace(cascade_path, path)
+
+        # now we have an array of all the cropped faces in crop_img and gray_img, which need to be fixed for size
+        # c
+        self.checkAndFixSize(crop_img)
+
+        print("writing cropped images into directories")
+
+        for i in range(0, len(crop_img), 1):
+            filename = "cropped_images/img{}.jpg".format(i)
+            filenamegray = "cropped_gray_images/grayimg{}.jpg".format(i)
+            cv.imwrite(filenamegray, crop_img[i])
+            cv.imwrite(filename, gray_img[i])
+
+        print("done saving images")
+
+    def checkAndFixSize(self, crop_img):
+
+        print("printing sizes of all images")
+
+        for i in range(len(crop_img)):
+            print("shape of the {}th image is".format(i+1))
+            print(crop_img[i].shape)
+            height, width, depth = crop_img[i].shape
+            imgScale = self.inputwidth / width
+            newX, newY = crop_img[i].shape[1] * imgScale, crop_img[i].shape[0] * imgScale
+            newimg = cv.resize(crop_img[i], (int(newX), int(newY)))
+            cv.imshow("Show by CV2", newimg)\
+
+
+
+
+
+if __name__ == "__main__":
+    d = faceDetect("first")
+    d.detectFaceOfImage(d.cascade_path, d.path)
+
+
 
 # cv.imshow('img',crop_img)
 # cv.waitKey(0)
 # cv.destroyAllWindows()
+
+
+
+
+
 
 
 
